95b5d1e2a05068156ce945084128fdf5698d94d2
jreyna13 <jreyna13@mail.stmarytx.edu>
2015-01-29 02:34:43 -0600

    Change2

--- First Project/src/HelloWorld.java
+++ First Project/src/HelloWorld.java
@@ -3,7 +3,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 
 		System.out.println("Hello WOrld!");
-		System.out.println("Change");
+		System.out.println("Change1");
+		System.out.println("change2");
 	}
 
 }
